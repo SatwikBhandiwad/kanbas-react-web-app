b86910aae26a791965e6c83b6e08090f6bf5066f
SatwikBhandiwad <satwik.bhandiwad@gmail.com>
2023-10-19 15:25:55 -0400

    Assignment 3 - third commit

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -8,23 +8,23 @@ function ModuleList() {
   const { courseId } = useParams();
   const modules = db.modules;
   return (
-    <div className="wd-flex-grow-1" style={{marginRight: 20}}>
-      <div style={{"float": "right"}}>
-        <button className="btn btn-light" onclick="location.href = '#'">
+    <div className="wd-flex-grow-1" style={{ marginRight: 20 }}>
+      <div style={{ "float": "right" }}>
+        <button className="btn btn-light wd-home-page-buttons me-2" onclick="location.href = '#'">
           Collapse All
         </button>
-        <button className="btn btn-light" onclick="location.href = '#'">
+        <button className="btn btn-light wd-home-page-buttons me-2" onclick="location.href = '#'">
           View Progress
         </button>
-        <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <FaCheckCircle className="far fa-check-circle" />
+        <button className="btn btn-light wd-home-page-buttons dropdown-toggle me-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <FaCheckCircle className="far fa-check-circle me-1" />
           Dropdown button
         </button>
-        <button class="btn btn-light wd-home-page-buttons" onclick="location.href = '#'" style={{ "color": "white", "background-color": "red" }}>
-          <FaPlus className="fas fa-plus" style={{color: "white"}} />
+        <button class="btn btn-light wd-home-page-buttons me-2" onclick="location.href = '#'" style={{ "color": "white", "background-color": "red" }}>
+          <FaPlus className="fas fa-plus" style={{ color: "white" }} />
           Module
         </button>
-        <button className="btn btn-light" onclick="location.href = '#'">
+        <button className="btn btn-light wd-home-page-buttons" onclick="location.href = '#'">
           <FaEllipsisV className="icon fa-ellipsis-v" style={{ "font-size": 25 }} />
         </button>
 
@@ -38,16 +38,22 @@ function ModuleList() {
             .filter((module) => module.course === courseId)
             .map((module, index) => (
               <React.Fragment key={index}>
-              <li className="list-group-item list-group-item-secondary" style={{borderRadius: 0}}>
-                <h3>{module.name}</h3>
-              </li>
-              <li className="list-group-item" style={{borderRadius: 0}}>
-                {module.description}
-              </li>
-              <br />
-              <br />
-            </React.Fragment>
-              
+                <li className="list-group-item list-group-item-secondary wd-module-items">
+                  {module.name}
+                  
+                  <FaEllipsisV className="icon fa-ellipsis-v wd-module-icons"/>
+                  <FaPlus className="fas fa-plus wd-module-icons" />
+                  <FaCheckCircle className="far fa-check-circle wd-module-icons" style={{color: "green"}} />
+                </li>
+                <li className="list-group-item wd-module-items" style={{ borderRadius: 0 }}>
+                  {module.description}
+                  <FaEllipsisV className="icon fa-ellipsis-v wd-module-icons"/>    
+                  <FaCheckCircle className="far fa-check-circle wd-module-icons" style={{color: "green"}} />
+                </li>
+                <br />
+                <br />
+              </React.Fragment>
+
             ))
         }
       </ul>
--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -5,6 +5,7 @@ import { FaBars } from "react-icons/fa";
 import { FaGlasses } from "react-icons/fa";
 import CourseNavigation from "./CourseNavigation";
 import Modules from "./Modules";
+import Home from "./Home";
 
 function Courses() {
     const { courseId } = useParams();
@@ -36,7 +37,7 @@ function Courses() {
                 >
                     <Routes>
                         <Route path="/" element={<Navigate to="Home" />} />
-                        <Route path="Home" element={<h1>Home</h1>} />
+                        <Route path="Home" element={<Home />} />
                         <Route path="Modules" element={<Modules/>} />
                         <Route path="Assignments" element={<h1>Assignments</h1>} />
                         <Route
