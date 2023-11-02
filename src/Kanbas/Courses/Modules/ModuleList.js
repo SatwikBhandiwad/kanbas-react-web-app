e5feed060f898fca2306352db0b0f4b303a033dd
SatwikBhandiwad <satwik.bhandiwad@gmail.com>
2023-11-02 17:51:53 -0400

    Assignment 4 - 3.3

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
 import { FaEllipsisV, FaPlus, FaCheckCircle, FaGripVertical } from "react-icons/fa";
 import "./index.css";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./modulesReducer";
+
 
 function ModuleList() {
   const { courseId } = useParams();
-  const modules = db.modules;
+  const modules = useSelector((state) => state.modulesReducer.modules);
+  const module = useSelector((state) => state.modulesReducer.module);
+  const dispatch = useDispatch();
+
+
+
   return (
     <div className="wd-flex-grow-1" style={{ marginRight: 20 }}>
       <div style={{ "float": "right" }}>
@@ -33,6 +46,32 @@ function ModuleList() {
       <br />
       <hr />
       <ul className="list-group">
+        <div>
+          <h3>Add/Update Module</h3>
+          <input value={module.name} className="form-control"
+            onChange={(e) =>
+              dispatch(setModule({ ...module, name: e.target.value }))
+
+            }
+          />
+          <br />
+          <textarea className="form-control" value={module.description}
+            onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))}
+          />
+          <br />
+          <button className="btn btn-success wd-home-page-buttons mb-4 me-3" style={{ "width": 60 }}
+            onClick={() => dispatch(addModule({ ...module, course: courseId }))}>
+            Add
+          </button>
+
+          <button className="btn btn-primary wd-home-page-buttons mb-4" style={{ "width": 80 }}
+            onClick={() => dispatch(updateModule(module))}>
+            Update
+          </button>
+        </div>
+
+
+
         {
           modules
             .filter((module) => module.course === courseId)
@@ -42,9 +81,24 @@ function ModuleList() {
                   <FaGripVertical className="me-2" />
                   {module.name}
 
+
+
                   <FaEllipsisV className="icon fa-ellipsis-v wd-module-icons" />
                   <FaPlus className="fas fa-plus wd-module-icons" />
                   <FaCheckCircle className="far fa-check-circle wd-module-icons" style={{ color: "green" }} />
+                  <button
+                    className="btn btn-success float-end me-4 wd-home-page-buttons"
+                    onClick={() => dispatch(setModule(module))}>
+                    Edit
+                  </button>
+
+
+                  <button
+                    className="btn btn-danger float-end me-4 wd-home-page-buttons"
+                    onClick={() => dispatch(deleteModule(module._id))}>
+                    Delete
+                  </button>
+
                 </li>
                 <li className="list-group-item wd-module-items" style={{ borderRadius: 0 }}>
                   <FaGripVertical className="me-2" />
